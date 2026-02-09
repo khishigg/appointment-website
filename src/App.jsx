52db6ba5ef6865e9ad438a0256983238135e097e
S.Khishigtogtokh <khishigtogtokh985@gmail.com>
2026-02-09 13:39:27 +0800

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MyNavbar from "./components/MyNavbar";
 import Footer from "./components/Footer";
-import BottomNav from "./components/ui/BottomNav";
 import Home from "./pages/Home";
-import Booking from "./pages/Booking";
 import BookingPage from "./pages/BookingPage";
 import CalendarPage from "./pages/CalendarPage";
 import Login from "./pages/Login";
-import PaymentUI from "./pages/PaymentUI";
-import AppointmentPage from "./pages/AppointmentPage";
-import RegisterPage from "./pages/RegisterPage";
-import ServiceChose from "./pages/ServiceChose";
-import AppointmentHistory from "./pages/AppointmentHistory";
-import AuthPage from "./pages/AuthPage";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 export default function App() {
@@ -22,15 +14,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/booking" element={<BookingPage />} />
-        <Route path="/emch-songoh" element={<Booking />} />
         <Route path="/calendar" element={<CalendarPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/service-chose" element={<ServiceChose />} />
-        <Route path="/_payment" element={<PaymentUI />} />
-        <Route path="/success" element={<AppointmentPage />} />
-        <Route path="/tuuh" element={<AppointmentHistory />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/register" element={<RegisterPage />} />
       </Routes>
       <Footer />
     </Router>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,12 @@
 import Hero from "../components/Hero";
-import HowToBook from "../components/HowToBook";
-import ExpertsSection from "../components/ExpertsSection";
-import FeaturesSection from "../components/FeaturesSection";
-import WhyChooseUs from "../components/WhyChooseUs";
 import PartnerLogos from "../components/PartnerLogos";
-import TrustStats from "../components/TrustStats";
-import ActiveCustomers from "../components/ActiveCustomers"; // Consider deleting if unused
+
 
 export default function Home() {
   return (
     <>
       <Hero />
       <PartnerLogos />
-
-
     </>
   );
 }
